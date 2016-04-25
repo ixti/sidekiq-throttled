ea54291907f891d5f7628fd0d08052a8e24ff864
Alexey Zapparov <ixti@member.fsf.org>
2016-04-25 00:24:57 +0000

    Update coverage init

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,10 +4,10 @@
 require "simplecov"
 require "coveralls"
 
-SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
   SimpleCov::Formatter::HTMLFormatter,
   Coveralls::SimpleCov::Formatter
-]
+])
 
 SimpleCov.start do
   add_filter "/spec/"
