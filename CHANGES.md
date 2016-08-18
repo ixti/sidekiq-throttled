9d732b3029ec764a73f9003158bd8a618b3fe998
Alexey Zapparov <ixti@member.fsf.org>
2016-08-18 02:37:28 +0000

    Release v0.5.0

--- CHANGES.md
+++ CHANGES.md
@@ -1,4 +1,4 @@
-## master (unreleased)
+## 0.5.0 (2016-08-18)
 
 * Drop Sidekiq 3.x support.
   ([@ixti])
--- lib/sidekiq/throttled/version.rb
+++ lib/sidekiq/throttled/version.rb
@@ -3,6 +3,6 @@
 module Sidekiq
   module Throttled
     # Gem version
-    VERSION = "0.4.0".freeze
+    VERSION = "0.5.0".freeze
   end
 end
