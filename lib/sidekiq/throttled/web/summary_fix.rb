5b002f2e6ed7f63e5d44a803a3faec2fcff2b180
Alexey Zapparov <ixti@member.fsf.org>
2017-04-02 11:21:31 +0000

    Fix enqueued link JS hack

--- lib/sidekiq/throttled/web/summary_fix.rb
+++ lib/sidekiq/throttled/web/summary_fix.rb
@@ -4,22 +4,14 @@ module Sidekiq
   module Throttled
     module Web
       module SummaryFix
-        JAVASCRIPT = <<-JAVASCRIPT
-        <script>
-          $(function ($el) {
-            var $el = $(".summary li.enqueued > a"),
-                url = $el.attr("href").replace(/\/queues$/, "/enhanced-queues");
-            $el.attr("href", url);
-          });
-        </script>
-        JAVASCRIPT
+        HTML = File.read("#{__dir__}/summary_fix.html").freeze
 
         class << self
           attr_accessor :enabled
         end
 
         def display_custom_head
-          SummaryFix.enabled ? "#{super}#{JAVASCRIPT}" : super
+          SummaryFix.enabled ? "#{super}#{HTML}" : super
         end
       end
     end
