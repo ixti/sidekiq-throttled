f35d460f867015620683bd8acf4cbb40e287d738
Alexey Zapparov <ixti@member.fsf.org>
2018-09-11 19:19:28 +0000

    Fix summary fix for sidekiq 5.2

--- lib/sidekiq/throttled/web/summary_fix.rb
+++ lib/sidekiq/throttled/web/summary_fix.rb
@@ -4,7 +4,8 @@ module Sidekiq
   module Throttled
     module Web
       module SummaryFix
-        HTML = File.read("#{__dir__}/summary_fix.html").freeze
+        JAVASCRIPT = [File.read(__FILE__.sub(/\.rb$/, ".js")).freeze].freeze
+        HEADERS    = { "Content-Type" => "application/javascript" }.freeze
 
         class << self
           attr_accessor :enabled
@@ -15,11 +16,22 @@ module Sidekiq
             else
               app.send(:prepend, SummaryFix)
             end
+
+            app.get("/throttled/summary_fix") do
+              [200, HEADERS.dup, JAVASCRIPT.dup]
+            end
           end
         end
 
         def display_custom_head
-          "#{super}#{HTML if SummaryFix.enabled}"
+          "#{super}#{summary_fix_script if SummaryFix.enabled}"
+        end
+
+        private
+
+        def summary_fix_script
+          src = "#{root_path}throttled/summary_fix"
+          %(<script type="text/javascript" src="#{src}"></script>)
         end
       end
     end
