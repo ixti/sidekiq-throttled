f1144c63a8d7642ae40d9a0017e7b4dbcc631b99
Charlie Jonas <charlie@charliejonas.co.uk>
2021-11-08 22:27:25 +0000

    Create Sidekiq::Throttled::Job alias

---
+++ lib/sidekiq/throttled/job.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require "sidekiq/throttled/worker"
+
+module Sidekiq
+  module Throttled
+    # A new module, Sidekiq::Job, was added in Sidekiq version 6.3.0 as a
+    # simple alias for Sidekiq::Worker as the term "worker" was considered
+    # too generic and confusing. Many people call a Sidekiq process a "worker"
+    # whereas others call the thread that executes jobs a "worker".
+    Job = Worker
+  end
+end
