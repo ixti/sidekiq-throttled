293942807524e6ef90d27ca8e7b3e867884543dd
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2021-12-05 18:00:42 +0900

    Fix test failure on `spec/sidekiq/throttled/strategy_collection_spec.rb`

--- spec/sidekiq/throttled/strategy_collection_spec.rb
+++ spec/sidekiq/throttled/strategy_collection_spec.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 RSpec.describe Sidekiq::Throttled::StrategyCollection do
-  let(:collection) { described_class.new(strategies, options) }
+  let(:collection) { described_class.new(strategies, **options) }
 
   let(:options) do
     {
