85ba5a456018ab7dedae93026e5f3318c11165a1
Paul A. Jungwirth <pj@illuminatedcomputing.com>
2021-12-22 10:35:21 -0800

    Report when a key_suffix proc raises an error

--- lib/sidekiq/throttled/strategy/base.rb
+++ lib/sidekiq/throttled/strategy/base.rb
@@ -12,7 +12,14 @@ module Sidekiq
 
         def key(job_args)
           key = @base_key.dup
-          key << ":#{@key_suffix.call(*job_args)}" if @key_suffix
+          if @key_suffix
+            begin
+              key << ":#{@key_suffix.call(*job_args)}"
+            rescue Exception => e
+              Sidekiq.logger.error "Failed to get key suffix: #{e}"
+              raise e
+            end
+          end
           key
         end
       end
