dd0f7714edf0ec76b94678d38df30876878c4db1
Malav Bhavsar <malav.bhavsar@gmail.com>
2023-07-25 19:46:24 -0700

    fix: middleware not finalizing correctly when job is ActiveJob

--- spec/lib/sidekiq/throttled/middleware_spec.rb
+++ spec/lib/sidekiq/throttled/middleware_spec.rb
@@ -30,6 +30,36 @@ RSpec.describe Sidekiq::Throttled::Middleware do
       end
     end
 
+    context "when job class has strategy with concurrency constraint and uses ActiveJob" do
+      let(:payload) do
+        {
+          "class"   => "ActiveJob::QueueAdapters::SidekiqAdapter::JobWrapper",
+          "wrapped" => "wrapped-foo",
+          "jid"     => "bar"
+        }
+      end
+
+      let! :strategy do
+        Sidekiq::Throttled::Registry.add payload["wrapped"],
+          concurrency: { limit: 1 }
+      end
+
+      it "calls #finalize! of queue with jid of job being processed" do
+        expect(strategy).to receive(:finalize!).with "bar"
+        middleware.call(double, payload, double) { |*| }
+      end
+
+      it "returns yields control to the given block" do
+        expect { |b| middleware.call(double, payload, double, &b) }
+          .to yield_control
+      end
+
+      it "returns result of given block" do
+        expect(middleware.call(double, payload, double) { |*| :foobar })
+          .to be :foobar
+      end
+    end
+
     context "when job class has strategy without concurrency constraint" do
       let! :strategy do
         Sidekiq::Throttled::Registry.add payload["class"],
