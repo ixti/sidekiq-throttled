a22338f5b810cf1ac22567f3aa5249d13683ade6
Connor Dunn <connor@found.com>
2023-10-17 12:03:16 -0700

    Add logic to temporarily skip queue if excessive number of throttled jobs

--- lib/sidekiq/throttled/patches/basic_fetch.rb
+++ lib/sidekiq/throttled/patches/basic_fetch.rb
@@ -2,6 +2,7 @@
 
 require "sidekiq"
 require "sidekiq/fetch"
+require "sidekiq/throttled/expirable_list"
 
 module Sidekiq
   module Throttled
@@ -13,6 +14,29 @@ module Sidekiq
           end
         end
 
+        # Timeout to sleep between fetch retries once at least
+        # timeout_after_attempts worth of throttled jobs are hit on a single
+        # queue. These balance scanning through a single queue for unthrottled
+        # jobs and not allowing lower priority queues to be completely starved
+        # by throttled jobs in higher queues
+        TIMEOUT_AFTER_ATTEMPTS = 100
+        TIMEOUT = 1
+
+        def initialize(cap)
+          super
+
+          sidekiq_version = Gem::Version.new(Sidekiq::VERSION)
+          if sidekiq_version < Gem::Version.new("7.0.0")
+            @throttled_queue_cooldown = config.fetch(:throttled_queue_cooldown, TIMEOUT)
+            @throttled_queue_after_attempts = config.fetch(:throttled_queue_after_attempts, TIMEOUT_AFTER_ATTEMPTS)
+          else
+            @throttled_queue_cooldown = config.lookup(:throttled_queue_cooldown) || TIMEOUT
+            @throttled_queue_after_attempts = config.lookup(:throttled_queue_after_attempts) || TIMEOUT_AFTER_ATTEMPTS
+          end
+
+          @paused = ExpirableList.new(@throttled_queue_cooldown)
+        end
+
         # Retrieves job from redis.
         #
         # @return [Sidekiq::Throttled::UnitOfWork, nil]
@@ -21,8 +45,25 @@ module Sidekiq
 
           if work && Throttled.throttled?(work.job)
             requeue_throttled(work)
+
+            if @last_throttled_queue == work.queue_name
+              @last_throttled_count += 1
+            else
+              @last_throttled_queue = work.queue_name
+              @last_throttled_count = 1
+            end
+
+            if @last_throttled_count >= @throttled_queue_after_attempts
+              @paused << work.queue_name
+              @last_throttled_queue = nil
+              @last_throttled_count = 0
+            end
+
             return nil
           end
+
+          @last_throttled_queue = nil
+          @last_throttled_count = 0
 
           work
         end
@@ -51,6 +92,8 @@ module Sidekiq
           # TODO: Refactor to be prepended as an integration mixin during configuration stage
           #   Or via configurable queues reducer
           queues -= Sidekiq::Pauzer.paused_queues.map { |name| "queue:#{name}" } if defined?(Sidekiq::Pauzer)
+
+          queues -= @paused.to_a
 
           queues
         end
